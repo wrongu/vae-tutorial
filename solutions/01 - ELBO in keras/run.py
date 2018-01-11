6da7756020571cd6ea6f75927a926b246d56ee4e
wrongu <rlange@ur.rochester.edu>
2018-01-11 15:54:55 -0500

    3-line run.py in each exercise; calls common visualize.py

--- solutions/01 - ELBO in keras/run.py
+++ solutions/01 - ELBO in keras/run.py
@@ -1,35 +1,10 @@
 from models import gaussian_mnist, fit_vae
-from data.my_mnist import x_train, x_test, img_rows, img_cols
-import matplotlib.pyplot as plt
-import numpy as np
-import keras.backend as K
+from data.my_mnist import x_train, x_test
+from visualize import render_grid
 
-# PART 1: CREATE AND TRAIN MODEL
-
+# Create and train the model
 vae = gaussian_mnist(latent_dim=2, pixel_std=.05)
-fit_vae(vae, x_train, x_test, epochs=100, weights_file='01-weights.h5')
+fit_vae(vae, x_train, x_test, epochs=100, weights_file='weights.h5')
 
-# PART 2: VISUALIZE RESULTS
-
-# Visualize results by running the P-model forward from a grid of points in the latent space
-# ranging from -3 to +3.
-num_grid = 15
-grid_pts = np.linspace(-3, 3, num_grid)
-
-# Create a keras function handle that takes latent points as input and produces images as output
-renderer = K.function([vae.latent.sample], [vae.reconstruction])
-latent_input = np.zeros((1, 2))  # must have shape (1, 2) not (2,)
-
-# Allocate space for the final (num_grid * rows, num_grid * cols) image
-final_image = np.zeros((num_grid * img_rows, num_grid * img_cols))
-
-# Populate each sub-image one at a time
-for i in range(num_grid):
-    for j in range(num_grid):
-        latent_input[:] = (grid_pts[i], grid_pts[j])
-        img = renderer([latent_input])[0]
-        final_image[j * img_rows:(j + 1) * img_rows, i * img_cols:(i + 1) * img_cols] = \
-            np.reshape(img, (img_rows, img_cols))
-
-plt.imshow(final_image, cmap='gray', extent=(-3, 3, -3, 3))
-plt.show()
+# Visualize results
+render_grid(vae)
---
+++ visualize.py
@@ -0,0 +1,29 @@
+from data.my_mnist import img_rows, img_cols
+import matplotlib.pyplot as plt
+import numpy as np
+import keras.backend as K
+
+
+def render_grid(vae):
+    # Visualize model by running the P-model forward from a grid of points in the latent space
+    # ranging from -3 to +3.
+    num_grid = 15
+    grid_pts = np.linspace(-3, 3, num_grid)
+
+    # Create a keras function that takes latent (x, y) as input and produces images as output
+    renderer = K.function([vae.latent.sample], [vae.reconstruction])
+    latent_input = np.zeros((1, 2))  # must have shape (1, 2) not (2,)
+
+    # Allocate space for the final (num_grid * rows, num_grid * cols) image
+    final_image = np.zeros((num_grid * img_rows, num_grid * img_cols))
+
+    # Populate each sub-image one at a time
+    for i in range(num_grid):
+        for j in range(num_grid):
+            latent_input[:] = (grid_pts[i], grid_pts[j])
+            img = renderer([latent_input])[0]
+            final_image[j * img_rows:(j + 1) * img_rows, i * img_cols:(i + 1) * img_cols] = \
+                np.reshape(img, (img_rows, img_cols))
+
+    plt.imshow(final_image, cmap='gray', extent=(-3, 3, -3, 3))
+    plt.show()

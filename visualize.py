1ea264284b884508d0e212eba263e11440dca30d
wrongu <rlange@ur.rochester.edu>
2018-02-09 13:08:21 -0500

    visualize.py can handle >2 dim latent. Used in cifar10 (exercise 05)

--- visualize.py
+++ visualize.py
@@ -1,10 +1,9 @@
-from data.my_mnist import img_rows, img_cols
 import matplotlib.pyplot as plt
 import numpy as np
 import keras.backend as K
 
 
-def render_grid(sample_var, reconstruction_var):
+def render_grid(sample_var, reconstruction_var, ndim=2, img_rows=28, img_cols=28, img_channels=1):
     # Visualize model by running the P-model forward from a grid of points in the latent space
     # ranging from -3 to +3.
     num_grid = 15
@@ -12,18 +11,18 @@ def render_grid(sample_var, reconstruction_var):
 
     # Create a keras function that takes latent (x, y) as input and produces images as output
     renderer = K.function([sample_var], [reconstruction_var])
-    latent_input = np.zeros((1, 2))  # must have shape (1, 2) not (2,)
+    latent_input = np.zeros((1, ndim))  # must have shape (1, ndim) not (ndim,)
 
     # Allocate space for the final (num_grid * rows, num_grid * cols) image
-    final_image = np.zeros((num_grid * img_rows, num_grid * img_cols))
+    final_image = np.zeros((num_grid * img_rows, num_grid * img_cols, img_channels))
 
     # Populate each sub-image one at a time
     for i in range(num_grid):
         for j in range(num_grid):
-            latent_input[:] = (grid_pts[i], grid_pts[j])
+            latent_input[0, :2] = (grid_pts[i], grid_pts[j])
             img = renderer([latent_input])[0]
-            final_image[j * img_rows:(j + 1) * img_rows, i * img_cols:(i + 1) * img_cols] = \
-                np.reshape(img, (img_rows, img_cols))
+            final_image[j * img_rows:(j + 1) * img_rows, i * img_cols:(i + 1) * img_cols, :] = \
+                np.reshape(img, (img_rows, img_cols, img_channels))
 
-    plt.imshow(final_image, cmap='gray', extent=(-3, 3, -3, 3))
+    plt.imshow(final_image.squeeze(), cmap='gray', extent=(-3, 3, -3, 3))
     plt.show()
